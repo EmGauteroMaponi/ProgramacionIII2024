17642f38889336df493692df4d9889794c385b18
Emanuel Gautero <emanuel.gautero@gmail.com>
2024-05-22 20:45:41 -0300

    Código base, para la resolución del parcial

--- src/main/java/org/escuela/programacionIII2024/ProgramacionIII2024.java
+++ src/main/java/org/escuela/programacionIII2024/ProgramacionIII2024.java
@@ -1,17 +1,38 @@
 package org.escuela.programacionIII2024;
 
+import org.escuela.programacionIII2024.modelo.Biblioteca;
+import org.escuela.programacionIII2024.modelo.Libro;
+import org.escuela.programacionIII2024.modelo.Persona;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Emanuel Gautero
  */
 public class ProgramacionIII2024 {
 
     public static void main(String[] args) {
-//        ListaEnteros listaEnteros = new ListaEnteros();  //Este es el objeto que hace una lista de enteros mediante un array
-        ListaEnteros listaEnteros = new EnterosConList(); //Este es el objeto que trabaja con una lista de entero mediante java.util.List
-        Primos primos =  new Primos(listaEnteros);
-//        InterfazUsuario interfazUsuario = new InterfazUsuario(primos); //Este es el objeto que presenta y pide los datos por Consola
-        InterfazUsuario interfazUsuario = new InterfazSwing(primos); //Este objeto presenta y pide los datos por medio de Java Swing
-        interfazUsuario.run();
+        List<Libro> libros = new ArrayList<>();
+        libros.add(new Libro("Libro1", new Persona("12123123","Autor 1"), "Ficción"));
+
+        List<Persona> personas = new ArrayList<>();
+        personas.add(new Persona("33345678", "cliente uno"));
+
+        Biblioteca biblioteca = new Biblioteca();
+
+        //Agregamos un libro nuevo a la biblioteca
+
+        //Eliminamos un libro de a biblioteca
+
+        //Buscamos un libro por el nombre
+
+        //Buscamos todos los libros por el nombre de un autor
+
+        //Agregamos a un cliente a la biblioteca
+
+        //Eliminamos a un cliente de la biblioteca
+
 
     }
 }
